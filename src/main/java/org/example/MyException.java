458a34995dcacfd717a242d8d43aad4d1f2fe334
DmytroBozhor <dimaastop1@gmail.com>
2023-11-23 18:47:52 +0100

    extending from throwable

---
+++ src/main/java/org/example/MyException.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MyException extends Throwable{
+
+    public MyException() {
+        super();
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+
+    public MyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyException(Throwable cause) {
+        super(cause);
+    }
+}
